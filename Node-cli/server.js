aca478a7bef457af0fb6625eff9d432cf91831a7
geezerP <mwaurameshack64@gmail.com>
2020-04-08 00:44:15 +0300

    ch:(add connection to the database )

--- Node-cli/server.js
+++ Node-cli/server.js
@@ -10,6 +10,22 @@ app.use(bodyparser.urlencoded({extended: true}));
 //parse requests of content-type - application/json which will be the case with our application
 app.use(bodyparser.json());
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 //define a simple route
 
 app.get('/', (req,res) => {
